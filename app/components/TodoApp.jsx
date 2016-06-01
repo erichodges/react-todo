0cff07fc2a0d050fb38261be5272a24542daf729
Eric Hodges <ehodges2004@gmail.com>
2016-06-01 15:20:37 -0700

    Added working checkboxes to todo items

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -12,19 +12,32 @@ var TodoApp = React.createClass({
       todos: [
         {
           id: uuid(),
-          text: 'Walk the dog'
+          text: 'Walk the dog',
+          completed: false
         }, {
           id: uuid(),
-          text: 'Clean the yard'
+          text: 'Clean the yard',
+          completed: true
         }, {
           id: uuid(),
-          text: 'Sell YHOO, lol'
+          text: 'Sell YHOO, lol',
+          completed: true
         }, {
           id: uuid(),
-          text: 'Buy?'
+          text: 'Buy?',
+          completed: false
         }
       ]
     };
+  },
+  handleToggle: function (id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    this.setState({todos: updatedTodos});
   },
   handleAddTodo: function(text) {
     this.setState({
@@ -32,7 +45,8 @@ var TodoApp = React.createClass({
         ...this.state.todos,
         {
           id: uuid(),
-          text: text
+          text: text,
+          completed: false
         }
       ]
     });
@@ -49,7 +63,7 @@ var TodoApp = React.createClass({
     return (
       <div>
         <TodoSearch onSearch={this.handleSearch}/>
-        <TodoList todos={todos}/>
+        <TodoList todos={todos} onToggle={this.handleToggle}/>
         <AddTodo onAddTodo={this.handleAddTodo}/>
       </div>
     )
