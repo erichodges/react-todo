154ddc66dc9a9235107982f9b55f098baaf80499
Eric Hodges <ehodges2004@gmail.com>
2016-06-07 13:05:08 -0700

    Actions test not working yet

--- webpack.config.js
+++ webpack.config.js
@@ -27,9 +27,9 @@ module.exports = {
       './app/components',
       './app/api'
     ],
-    alias: {
-      Main: 'app/components/Main.jsx',
-      applicationStyles: 'app/styles/app.scss'
+    alias: {      
+      applicationStyles: 'app/styles/app.scss',
+      actions: 'app/actions/actions.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
