c113a6a129698cbdf73884140593a4087e71ee27
Eric Hodges <ehodges2004@gmail.com>
2016-06-07 13:40:50 -0700

    Actions test still not showing

--- app/tests/actions/actions.text.jsx
+++ app/tests/actions/actions.text.jsx
@@ -1,7 +1,7 @@
 var expect = require('expect');
 var actions = require('actions');
 
-describe('Actions', () {
+describe('Actions', () => {
   it('Should generate search text action', () => {
     var action = {
       type: 'SET_SEARCH_TEXT',
