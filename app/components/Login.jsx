858582207ec5838f18ee7f5f18fcb8ba38ea0afa
Eric Hodges <ehodges2004@gmail.com>
2016-06-16 14:11:29 -0700

    Added actions for firebase login and logout

--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import * as Redux from 'react-redux';
+
+import * as actions from 'actions';
 
 export var Login = React.createClass({
+  onLogin() {
+    var {dispatch} = this.props;
+
+    dispatch(actions.startLogin());
+  },
   render() {
     return (
       <div>
@@ -13,7 +21,7 @@ export var Login = React.createClass({
               <p>
                 Login with GitHub account below
               </p>
-              <buton className="button">Login With GitHub</buton>
+              <buton className="button" onClick={this.onLogin}>Login With GitHub</buton>
             </div>
           </div>
         </div>
@@ -22,4 +30,4 @@ export var Login = React.createClass({
   }
 });
 
-export default Login;
+export default Redux.connect()(Login);
