610079066f6865b11f383f5ae5ac014ef565300c
Eric Hodges <ehodges2004@gmail.com>
2016-06-17 15:27:52 -0700

    Added react-router middleware and redirect on auth

--- app/app.jsx
+++ app/app.jsx
@@ -1,14 +1,12 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var {Provider} = require('react-redux');
-var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+var {hashHistory} = require('react-router');
 
 var actions = require('actions');
 var store = require('configureStore').configure();
-var TodoAPI = require('TodoAPI');
-import Login from 'Login';
-import TodoApp from 'TodoApp';
 import firebase from 'app/firebase/';
+import router from 'app/router/';
 
 firebase.auth().onAuthStateChanged((user) => {
   if (user) {
@@ -18,8 +16,6 @@ firebase.auth().onAuthStateChanged((user) => {
   }
 });
 
-
-
 store.dispatch(actions.startAddTodos());
 
 // Load foundation
@@ -28,28 +24,9 @@ $(document).foundation();
 // App scss
 require('style!css!sass!applicationStyles');
 
-var requireLogin = (nextState, replace, next) => {
-  if (!firebase.auth().currentUser) {
-    replace('/');
-  }
-  next();
-};
-
-var redirectIfLoggedIn = (nextState, replace, next) => {
-  if (firebase.auth().currentUser) {
-    replace('/todos/');
-  }
-  next();
-};
-
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={hashHistory}>
-      <Route path="/">
-        <Route path="todos" component={TodoApp} onEnter={requireLogin}/>
-        <IndexRoute component={Login} onEnter={redirectIfLoggedIn}/>
-      </Route>
-    </Router>
+    {router}
   </Provider>,
   document.getElementById('app')
 );
