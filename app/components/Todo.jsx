5084e9ca8885baa1e8da9f151b89240a28611c30
Eric Hodges <ehodges2004@gmail.com>
2016-06-28 14:22:32 -0700

    Added deleteTodo styles

--- app/components/Todo.jsx
+++ app/components/Todo.jsx
@@ -4,13 +4,23 @@ var moment = require('moment');
 var actions = require('actions');
 
 export var Todo = React.createClass({
+  componentDidUpdate: function () {
+        console.log('update');
+        var input = ReactDom.findDOMNode(this.refs.editText);
+        if(input) {
+            input.focus();
+        }
+    },
   render: function(){
     var {id, text, completed, createdAt, completedAt, dispatch} = this.props;
     var todoClassName = completed ? 'todo todo-completed' : 'todo';
     var renderDate = () => {
       var message = 'Created ';
       var timestamp = createdAt;
-
+      if (edited) {
+                message = "Edited ";
+                timestamp = editedAt;
+            }
       if (completed) {
         message = 'Completed ';
         timestamp = completedAt;
@@ -18,17 +28,47 @@ export var Todo = React.createClass({
 
       return message + moment.unix(timestamp).format('MMM Do YYYY @ h:mm a');
     };
+
+    var renderTodo = () => {
+            if(!edit) {
+                return <p className="todo-text">{text}</p>;
+            } else {
+                return <form onSubmit={(e)=> {
+                    e.preventDefault();
+                    var editedText = this.refs.editText.value;
+                    dispatch(actions.editTodo(id, editedText));
+                }}><input type="text" ref="editText" defaultValue={text} onBlur={()=> {
+                    dispatch(actions.editTodo(id, ''));
+                }} autofocus id="editInput"/>
+                </form>
+            }
+        };
+
+        var renderEdit = () => {
+            if(!edit) {
+                return <button className="button alert del-btn" ref="delBtn" onClick={() => {
+                    dispatch(actions.deleteTodo(id));
+                }}>X</button>;
+            }
+            return;
+        }
+
     return (
-      <div className={todoClassName} onClick={() => {
-          dispatch(actions.startToggleTodo(id, !completed));
-        }}>
-        <div>
-          <input type="checkbox" checked={completed}/>
-        </div>
-        <div>
-          <p>{text}</p>
-          <p className="todo__subtext">{renderDate()}</p>
-        </div>
+      <div className={todoClassName} >
+        	<div>
+        		<input type="checkbox" onClick={() => {
+            dispatch(actions.toggleTodo(id))
+            }} checked={completed} ref="todoCheckbox"/>
+        	</div>
+        	<div onClick={() => {
+                dispatch(actions.editTodo(id, ''))
+            }} ref="todo">
+                {renderTodo()}
+        		<p className="todo_subtext">{renderDate()}</p>
+        	</div>
+            <div className="todoBtns">
+              {renderEdit()}
+            </div>
       </div>
     )
   }
